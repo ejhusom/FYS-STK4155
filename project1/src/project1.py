a2930bc939d59ea15111f15e407f2d058f4a62ce
ejhusom <ejlgkvam@online.no>
2019-09-11 13:51:24 +0200

    Done with ex b

--- project1/src/project1.py
+++ project1/src/project1.py
@@ -26,80 +26,81 @@ class RegressionResampling():
 
         self.n = n
         self.deg = deg
+        self.p = int((self.deg+1)*(self.deg+2)/2)
         self.x, self.y = self.generate_xy()
         self.z = self.franke_function()
         self.X = self.create_design_matrix()
 
-        self.beta_OLS = self.OLS()
-        self.z_OLS = self.X @ self.beta_OLS
-
-        print(np.shape(self.z))
-        print(np.shape(self.z_OLS))
-        self.print_error_analysis(np.ravel(self.z), self.z_OLS)
 
     def franke_function(self, eps = 0.05):
 
         np.random.seed(0)
 
-        x = self.x
-        y = self.y
+        x = np.reshape(self.x, (self.n, self.n))
+        y = np.reshape(self.y, (self.n, self.n))
 
         term1 = 0.75*np.exp(-(0.25*(9*x-2)**2) - 0.25*((9*y-2)**2))
         term2 = 0.75*np.exp(-((9*x+1)**2)/49.0 - 0.1*(9*y+1))
         term3 = 0.5*np.exp(-(9*x-7)**2/4.0 - 0.25*((9*y-3)**2))
         term4 = -0.2*np.exp(-(9*x-4)**2 - (9*y-7)**2)
         
-        return term1 + term2 + term3 + term4 + eps*np.random.randn(self.n)
+        z = term1 + term2 + term3 + term4 + eps*np.random.randn(self.n)
+
+        return np.ravel(z)
 
 
     def generate_xy(self, start = 0, stop = 1):
+        '''Generate x and y data and return at as a flat meshgrid.'''
 
         x = np.linspace(start, stop, self.n)
         y = np.linspace(start, stop, self.n)
         x, y = np.meshgrid(x, y)
+        
+        return np.ravel(x), np.ravel(y)
 
-        return x, y
 
     def create_design_matrix(self):
 
-        if len(self.x.shape) > 1:
-                x_rvl = np.ravel(self.x)
-                y_rvl = np.ravel(self.y)
-
-        N = len(x_rvl)
-        p = int((self.deg+1)*(self.deg+2)/2)
-        X = np.ones((N,p))
+        N = len(self.x)
+        X = np.ones((N,self.p))
 
         for i in range(1, self.deg+1):
             q = int((i)*(i+1)/2)
             for k in range(i+1):
-                X[:,q+k] = x_rvl**(i-k) * y_rvl**k
+                X[:,q+k] = self.x**(i-k) * self.y**k
 
         return X
 
 
-    def OLS(self):
+    def regression(self, X, z, _lambda=0):
 
-        z_rvl = np.ravel(self.z)
-        X = self.X
-        beta = np.linalg.pinv(X.T.dot(X)).dot(X.T).dot(z_rvl)
-        
-        return beta
+        beta = np.linalg.pinv(X.T.dot(X) + \
+            _lambda*np.identity(self.p)).dot(X.T).dot(z)
+        z_tilde = X @ beta
+
+        return beta, z_tilde
 
 
     def print_error_analysis(self, z, z_tilde):
         '''Print error analysis of regression fit using scikit.'''
         
-        print("Mean squared error: %.2f" % mean_squared_error(z, z_tilde))
-        print('R2 score: %.2f' % r2_score(z, z_tilde))
-        print('Mean absolute error: %.2f' % mean_absolute_error(z, z_tilde))
+        print("Mean squared error: %.8f" % mean_squared_error(z, z_tilde))
+        print('R2 score: %.8f' % r2_score(z, z_tilde))
+        print('Mean absolute error: %.8f' % mean_absolute_error(z, z_tilde))
 
 
     def cross_validation(self):
 
-        k = 5
+        kf = KFold(n_splits=5, random_state=0, shuffle=True)
 
-        for
+        for train_index, test_index in kf.split(self.X):
+
+            beta, z_OLS = self.regression(self.X[train_index], self.z[train_index],
+                    _lambda=0)
+
+            z_tilde = self.X[test_index] @ beta
+
+            self.print_error_analysis(self.z[test_index], z_tilde)
 
 
     def plot_franke(self):
@@ -107,7 +108,8 @@ class RegressionResampling():
         fig = plt.figure()
         ax = fig.gca(projection='3d')
 
-        surf = ax.plot_surface(self.x, self.y, self.z, cmap=cm.coolwarm,linewidth=0, antialiased=False)
+        surf = ax.plot_surface(self.x, self.y, self.z,
+                cmap=cm.coolwarm,linewidth=0, antialiased=False)
         
         # Customize the z axis.
         ax.set_zlim(-0.10, 1.40)
@@ -119,6 +121,19 @@ class RegressionResampling():
         plt.show()
 
 
-if __name__ == '__main__': 
-    proj = RegressionResampling()
+    def ex_a(self):
 
+        beta, z_tilde = self.regression(self.X, self.z, _lambda=0)
+        self.print_error_analysis(self.z, z_tilde)
+
+
+    def ex_b(self):
+
+        self.cross_validation()
+
+
+if __name__ == '__main__': 
+    project1 = RegressionResampling()
+
+    project1.ex_a()
+    project1.ex_b()
