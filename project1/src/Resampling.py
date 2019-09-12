b85f4b914c964d5567f52788793f7609016331b0
ejhusom <ejlgkvam@online.no>
2019-09-12 13:38:25 +0200

    Add calculation of MSE and R2

--- project1/src/Resampling.py
+++ project1/src/Resampling.py
@@ -16,6 +16,10 @@ class Resampling(Regression):
 
         kf = KFold(n_splits=n_folds, random_state=0, shuffle=True)
 
+        mse = np.zeros(n_folds)
+        r2 = np.zeros(n_folds)
+
+        i = 0
         for train_index, test_index in kf.split(self.X):
 
             if method==0:
@@ -27,4 +31,10 @@ class Resampling(Regression):
 
             z_tilde = self.X[test_index] @ self.beta
 
-            self.print_error_analysis()
+            mse[i] = mean_squared_error(self.z, z_tilde)
+            r2[i] = r2_score(self.z, z_tilde)
+
+            i += 1
+
+        self.mse_cv = np.mean(mse)
+        self.r2.cv = np.mean(r2)
