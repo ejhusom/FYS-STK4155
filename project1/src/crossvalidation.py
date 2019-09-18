Plot bias variance, seems wrong

project1/src/crossvalidation.py
@@ -19,6 +19,8 @@ def cross_validation(X, y, n_folds, method='ols', lambda_=0.01):
 
     mse = np.zeros((n_folds, 2))
     r2 = np.zeros((n_folds, 2))
+    b = np.zeros((n_folds, 2))
+    var = np.zeros((n_folds, 2))
 
     
 
@@ -38,6 +40,10 @@ def cross_validation(X, y, n_folds, method='ols', lambda_=0.01):
         mse[i][1] = mean_squared_error(y[test_index], y_pred_test)
         r2[i][0] = r2_score(y[train_index], y_pred_train)
         r2[i][1] = r2_score(y[test_index], y_pred_test)
+        b[i][0] = bias(y[train_index], y_pred_train)
+        b[i][1] = bias(y[test_index], y_pred_test)
+        var[i][0] = np.var(y_pred_train)
+        var[i][1] = np.var(y_pred_test)
 
         i += 1
 
@@ -45,6 +51,10 @@ def cross_validation(X, y, n_folds, method='ols', lambda_=0.01):
     mse_test = np.mean(mse[:,1])
     r2_train = np.mean(r2[:,0])
     r2_test = np.mean(r2[:,1])
+    b_train = np.mean(b[:,0])
+    b_test = np.mean(b[:,1])
+    var_train = np.mean(var[:,0])
+    var_test = np.mean(var[:,1])
 
 
-    return mse_train, mse_test, r2_train, r2_test
+    return mse_train, mse_test, r2_train, r2_test, b_train, b_test, var_train, var_test

project1/src/regression_analysis.py
@@ -20,56 +20,23 @@ from franke import *
 from designmatrix import *
 
 
-
-
-def analyze_regression(x1, x2, y, method='ridge'):
-
-    max_degree = 10
-
-
-    error_scores = pd.DataFrame(columns=['degree', 'MSE', 'R2', 'bias',
-    'variance'])
-
-    for deg in range(1, max_degree+1):
-
-        X = create_design_matrix(x1, x2, deg=deg)
-        model = Regression(method, lambda_=0.01)
-        model.fit(X, y)
-        model.predict(X)
-
-        error_scores = error_scores.append({'degree': deg, 
-                                            'MSE': mean_squared_error(model.y,
-                                                model.y_pred), 
-                                            'R2': r2_score(model.y,
-                                                model.y_pred),
-                                            'bias': bias(model.y,
-                                                model.y_pred),
-                                            'variance':
-                                            variance(model.y_pred)},
-                                            ignore_index=True)
-
-    
-    print(f'Analyzing {method}:')
-    print(error_scores)
-    error_scores.to_csv(f'error_scores_{method}.csv')
-
-
-
-
-
-
-def analyze_regression_cv(x1, x2, y, method='ols', n_folds=5, data_name='data'):
+def analyze_regression(x1, x2, y, method='ols', n_folds=5, data_name='data'):
 
     max_degree = 10
     n_lambdas = 6
     lambdas = np.logspace(-3, 0, n_lambdas)
 
     error_scores = pd.DataFrame(columns=['degree', 'lambda', 'MSE_train',
-        'MSE_test', 'R2_train', 'R2_test', 'bias', 'variance'])
+        'MSE_test', 'R2_train', 'R2_test', 'bias_train', 'bias_test',
+        'var_train', 'var_test'])
 
     if method=='ols':
         lambdas = [0]
+    
+    filename = 'error_scores_' + data_name + '_' + method
 
+    if n_folds > 1: 
+        filename += '_cv'
 
     for lambda_ in lambdas:
         for deg in range(1, max_degree+1):
@@ -77,17 +44,22 @@ def analyze_regression_cv(x1, x2, y, method='ols', n_folds=5, data_name='data'):
             X = create_design_matrix(x1, x2, deg=deg)
 
             if n_folds > 1:
-                mse_train, mse_test, r2_train, r2_test = cross_validation(X, y,
-                        n_folds)
-
+                mse_train, mse_test, r2_train, r2_test, bias_train, bias_test, var_train, var_test = cross_validation(X, y, n_folds)
+                
+                
             else:
                 model = Regression(method, lambda_=0.01)
                 model.fit(X, y)
                 model.predict(X)
                 mse_train = mean_squared_error(model.y, model.y_pred)
                 r2_train = r2_score(model.y, model.y_pred)
+                bias_train = bias(model.y, model.y_pred)
+                var_train = np.var(model.y_pred)
                 mse_test = None
                 r2_test = None
+                bias_test = None
+                var_test = None
+
 
 
             error_scores = error_scores.append({'degree': deg, 
@@ -95,21 +67,24 @@ def analyze_regression_cv(x1, x2, y, method='ols', n_folds=5, data_name='data'):
                                                 'MSE_train': mse_train, 
                                                 'MSE_test': mse_test,
                                                 'R2_train': r2_train, 
-                                                'R2_test': r2_test},
+                                                'R2_test': r2_test,
+                                                'bias_train': bias_train,
+                                                'bias_test': bias_test,
+                                                'var_train': var_train,
+                                                'var_test': var_test},
                                                 ignore_index=True)
 
     
 
     print(error_scores)
-    error_scores.to_csv(f'error_scores_{data_name}_{method}_cv.csv')
+    error_scores.to_csv(filename + '.csv')
 
 
 def franke_regression():
     x1, x2 = generate_mesh(0, 1, 100)
-    y = franke_function(x1, x2, eps=1)
+    y = franke_function(x1, x2, eps=0.1)
 
-    analyze_regression_cv(x1, x2, y, n_folds=1, method='ols', data_name='franke')
-    analyze_regression(x1, x2, y, method='ols')
+    analyze_regression(x1, x2, y, n_folds=5, method='ols', data_name='franke')
     
 
 def terrain_regression(terrain_file, plot=0):
@@ -131,12 +106,17 @@ def plot_regression_analysis(filename):
 
     plt.figure()
 
-    for lambda_ in lambdas:
-        dfl = df.loc[df['lambda'] == lambda_]
-        plt.plot(dfl['degree'], dfl['MSE_train'], label=f'train, \
-                lambda={lambda_}')
-        plt.plot(dfl['degree'], dfl['MSE_test'], label=f'test, \
-                lambda={lambda_}')
+#    for lambda_ in lambdas:
+#        dfl = df.loc[df['lambda'] == lambda_]
+#        plt.plot(dfl['degree'], dfl['MSE_train'], label=f'train, \
+#                lambda={lambda_}')
+#        plt.plot(dfl['degree'], dfl['MSE_test'], label=f'test, \
+#                lambda={lambda_}')
+
+    plt.plot(df['degree'], df['bias_test'], label='bias')
+    plt.plot(df['degree'], df['var_test'], label='variance')
+    plt.plot(df['degree'], df['MSE_test'], label='error')
+
 
     plt.legend()
 
@@ -149,4 +129,4 @@ if __name__ == '__main__':
 
     #terrain_regression('dat/n27_e086_1arc_v3.tif', plot=1)
     franke_regression()
-    plot_regression_analysis('error_scores_franke_ridge_cv.csv')
+    plot_regression_analysis('error_scores_franke_ols_cv.csv')