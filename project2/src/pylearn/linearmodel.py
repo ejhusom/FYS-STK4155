4d77255da3bb7cf04a115b7e6366ce8799dc71af
ejhusom <ejlgkvam@online.no>
2019-10-23 20:23:48 +0200

    Remove unnecessary ravel and import

--- project2/src/pylearn/linearmodel.py
+++ project2/src/pylearn/linearmodel.py
@@ -9,7 +9,6 @@
 # ============================================================================
 import numpy as np
 import sklearn.linear_model as skl
-from sklearn.metrics import mean_squared_error, r2_score
 
 
 class Regression():
@@ -31,9 +30,6 @@ class Regression():
 
     def fit(self, X, y):
         
-        if len(np.shape(y)) > 1:
-            y = np.ravel(y)
-
         self.X = X
         self.y = y
 
@@ -64,21 +60,11 @@ class Regression():
 
     def ridge(self):
 
-        #X_center = self.X - np.mean(self.X, axis=0)
-        #y_center = self.y - np.mean(self.y)
+        X = self.X
 
-# TODO: Normalize
-#        col_var = np.var(self.X, axis=0)
-#
-#        for i in range(1, len(self.X[0,:])):
-#            self.X[:,i] /= col_var[i]
-#
-#        self.X /= np.var(self.X, axis=0)
-#        self.y /= np.var(self.y)
-
-        #X = X_center
-        self.beta = np.linalg.pinv(X.T.dot(X) + \
-            self.lambda_*np.identity(np.shape(self.X)[1])) @ X.T @ y_center
+        self.beta = (np.linalg.pinv(X.T.dot(X) +
+                    self.lambda_*np.identity(np.shape(self.X)[1])) @ X.T @
+                    self.y)
 
     def lasso(self):
 
