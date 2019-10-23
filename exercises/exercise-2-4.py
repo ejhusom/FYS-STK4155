b867c84407d932615356552c236b348c6e9a1418
ejhusom <ejlgkvam@online.no>
2019-10-23 20:28:17 +0200

    Remove jupyter notebook, replace with standard python file

--- exercises/exercise-2-4.py
+++ exercises/exercise-2-4.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# # Exercise 2
+
+# In[1]:
+
+
+# Exercise 2.1: Own code for making polynomial fit
 import numpy as np
 import matplotlib.pyplot as plt
 import sklearn.linear_model as skl
@@ -14,7 +20,6 @@ except:
 
 def plot_regression(x, y, x_fit, y_fit, y_label='model fit'):
     '''Plot data and model fit in one figure.'''
-
     plt.figure()
     plt.plot(x, y, '.', label='random data')
     plt.plot(x[idx_sorted], y_tilde[idx_sorted], '-', label=y_label)
@@ -23,28 +28,22 @@ def plot_regression(x, y, x_fit, y_fit, y_label='model fit'):
     
 def print_error_analysis(y, y_tilde):
     '''Print error analysis of regression fit using scikit.'''
-    
     print("Mean squared error: %.2f" % mean_squared_error(y, y_tilde))
     print('R2 score: %.2f' % r2_score(y, y_tilde))
     print('Mean absolute error: %.2f' % mean_absolute_error(y, y_tilde))
 
-
-# Data generation
 N = 100 # data size
 p = 2   # polynomial degree
 
+# Random data
 x = np.random.rand(N, 1)
 y = 5*x*x + 0.1*np.random.randn(N, 1)
-
 
 # Creating design matrix X
 X = np.ones((N, p + 1))
 for i in range(1, p + 1):
     X[:,i] = x[:,0]**i
     
-
-# Exercise 2.1: Own code for making polynomial fit
-
 # Matrix inversion
 beta = np.linalg.inv(X.T.dot(X)).dot(X.T).dot(y)
 # Creating model
@@ -53,7 +52,6 @@ y_tilde = X @ beta
 # Sorting arrays for plotting
 idx_sorted = np.argsort(x[:,0])
 
-
 plot_regression(x, y, 
                 x[idx_sorted], y_tilde[idx_sorted], 
                 y_label='polynomial fit degree 2')
@@ -61,7 +59,7 @@ plot_regression(x, y,
 print_error_analysis(y, y_tilde)
 
 
-# In[19]:
+# In[2]:
 
 
 # Exercise 2.2: Using scikit-learn to make polynomial fit
@@ -69,6 +67,7 @@ print_error_analysis(y, y_tilde)
 clf = skl.LinearRegression().fit(X, y)
 y_tilde_scikit = clf.predict(X)
 
+# Plotting model and data
 plot_regression(x, y, 
                 x[idx_sorted], y_tilde_scikit[idx_sorted], 
                 y_label='scikit polynomial fit degree 2')
@@ -76,6 +75,12 @@ plot_regression(x, y,
 # Exercise 2.3: Error analysis.
 print_error_analysis(y, y_tilde_scikit)
 
+
+# # Exercise 4
+# 
+# Continuation of exercise 2. Writing code for Ridge and Lasso regression.
+
+# In[3]:
 
 
 # Exercise 4.1: Own code for ridge regression.
@@ -99,4 +104,48 @@ plot_regression(x, y,
                 y_label='scikit ridge regression')
 
 print_error_analysis(y, y_ridge_scikit)
+
+
+# # Exercise 4.3: Analyze parameters beta
+# 
+# We want to analyze the parameters $\beta_1$ and $\beta_2$. 
+
+# In[4]:
+
+
+def variance(x):
+    '''Calculate variance of an array.'''
+    return np.mean(np.abs(x - x.mean())**2)
+
+def variance_beta_ols(X):
+    '''Calculate the variance of beta for OLS.'''
+    return np.linalg.inv(X.T.dot(X))*variance(y)
+
+def variance_beta_ridge(X):
+    '''Calculate the variance of beta for Ridge regression.'''
+    return variance(y)*np.linalg.inv(X.T.dot(X) + _lambda*np.identity(p+1)).dot(X.T.dot(X)).dot(np.linalg.inv(X.T.dot(X) + _lambda*np.identity(p+1)))
+
+print('Linear regression, variance of beta parameters:')
+for i in range(p+1):
+    print(variance_beta_ols(X)[i][i])
+    
+print('\nRidge regression, variance of beta parameters:')
+for i in range(p+1):
+    print(variance_beta_ridge(X)[i][i])
+
+
+# In[7]:
+
+
+# Exercise 4.4 and 4.5
+clf_lasso = skl.Lasso(alpha=_lambda).fit(X, y)
+y_lasso_scikit = clf_lasso.predict(X)
+print(clf_lasso.coef_)
+print(clf_lasso.intercept_)
+
+plot_regression(x, y, 
+                x[idx_sorted], y_lasso_scikit[idx_sorted], 
+                y_label='scikit lasso regression')
+
+print_error_analysis(y, y_lasso_scikit)
 
