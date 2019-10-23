6baa7393f96a66818947d4ce44c7f07fbbcb1bf3
ejhusom <ejlgkvam@online.no>
2019-10-23 21:24:54 +0200

    Experiment with analysis

--- project2/src/franke_analysis.py
+++ project2/src/franke_analysis.py
@@ -9,7 +9,6 @@
 # ============================================================================
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.metrics import mean_squared_error, r2_score, accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
